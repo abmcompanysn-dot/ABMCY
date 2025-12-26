1b3aeb396022b9e9a955ad5564ef294e7730ca9b
abmcompanysn-dot <abmcompanysn@gmail.com>
2025-12-26 19:16:23 +0000

    Message clair sur les modifications

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
     // IMPORTANT : Remplacez ceci par l'URL de votre application web Apps Script déployée
-    scriptUrl: "https://script.google.com/macros/s/AKfycbw7ONW9FQtZMsqPgkNAXGHqwGC3PDG-AaNY7Xlh2ItuFjzxeV1GP2gh-3CBPdPeKwlxfA/exec",
+    scriptUrl: "https://script.google.com/macros/s/AKfycbzdM_AkQoxz9vyCc3_auMmp5y9sD1uMLWLAICbqDBvpxwyzpkpPx99P2eeIyx5zs248kQ/exec",
 
     // Liens vers vos plateformes externes (pour les boutons "Visiter")
     externalPlatformLinks: {
